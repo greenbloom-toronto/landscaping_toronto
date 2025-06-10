18f257a0a8fd4b4ebd452012dc976876b587fbf3
salt-viktor <viktor.riabokon@ilovesalt.com>
2025-06-10 02:03:29 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import svgr from '@svgr/rollup'
 
 export default defineConfig({
+  base: '/landscaping_toronto/',
   plugins: [
     react(),
     svgr() // 👈 this must be here
